89831abac5a5724dd81c6a5e41a27f0a88123965
Ildsjel <jo-hoenscheid@t-online.de>
2018-11-02 19:44:29 +0100

    merged FE files into view folder to achieve mvc structure

--- CoD_v4/app.js
+++ CoD_v4/app.js
@@ -1,5 +1,3 @@
-
-
 // app.js
 
 const express = require("express"); /*express = Uncaught reference error - Needs to be fixed. This has to run in node. javascript does not understand */
@@ -7,20 +5,22 @@ const bodyParser = require("body-parser");
 const taskController = require("./controller/TaskController");
 const WSJFController = require("./controller/WSJFController");
 
-
-
-
 // db instance connection
-require ("./config/db");
-const port = 3306;
+require ("./config/db.js");
+const port = 3308;
+
+//invoke express app from the above require statement for express
 const app = express();
 
 // const port = process.env.PORT || 3304,
 mongoose = require ("mongoose");
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost:3306/node-demo");
-app.use(bodyParser.urlencoded({ extended: true })); /*bodyParser is not defined: I NEED TO FIX THAT*/
+mongoose.connect("mongodb://localhost:3308/test");
+/*
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+//bodyParser is not defined: I NEED TO FIX THAT
+*/
 
 // API ENDPOINTS
 
@@ -36,7 +36,7 @@ app
   .delete(taskController.deleteTask);
 /* I NEED TO FINISH THIS. I CREATED A FILE CALLED WSJFitem.js in the model folder to define the model and pass the form data in the mongodb. The model route is defined in the html file in the form as action attribute
   */
-app
+app.post('/', (req, res) => res.send('Item successfully created'))
     .route("/post_WSJF_item")
     .get(WSJFController.listAllWSJFtems)
     .post(WSJFController.createWSJFItem);
@@ -47,7 +47,7 @@ app
 app
   .route("/post_WSJF_item/:WSJF_item_id")
 
-app.get('/', (req, res) => res.send('Hello World!'))
+//app.get('/', (req, res) => res.send('Hello World!')) //this is leading to the result of having hello world printed on the localhost
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
